b4023e0e6c2b7b355774938bc8656e12a30f27f6
Rajneesh Kumar <rajneeshkumar2093@gmail.com>
2025-06-20 19:24:05 +0530

    added session storage instead of local storage

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,10 +1,11 @@
 // frontend/utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000"); // Make sure backend is running on this
+const socket = io(`${import.meta.env.VITE_API_URL}`); // Make sure backend is running on this
 
 socket.on("connect", () => {
-  console.log("✅ Connected to socket:", socket.id);
+  console.log(" Connected to socket:", socket.id);
 });
 
 export { socket };
+
